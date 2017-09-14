8c1ac19d68d559038368cad9656302eda6f58e08
Aakash Patel <aakash.15patel@gmail.com>
2017-09-14 00:12:36 -0400

    Added one method named as Method1(String a) { }

---
+++ app/src/main/java/com/example/aakash/versiona/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.aakash.versiona;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void method1(String a)
+    {
+        /*comment*/
+    }
+
+}
